1a2daca8b59aa9b1ae4b8cc22089d9cb8453d91a
Parker Piedmont <pjpiedmont@gmail.com>
2021-12-01 02:31:17 -0800

    Added documentation and made names more descriptive

--- adcs/src/main.cpp
+++ adcs/src/main.cpp
@@ -1,181 +1,274 @@
+#include "comm.h"
 #include "ICM_20948.h"
 #include <FreeRTOS_SAMD51.h>
+#include <stdint.h>
 
-typedef union
-{
-	uint8_t data[11];
-
-	struct
-	{
-		uint8_t status;
-		uint8_t voltage;
-		uint8_t current;
-		uint8_t speed;
-		uint8_t mag[3];
-		uint8_t gyro[3];
-	};
-} TESdata;
-
-#define COMMAND_LEN  2
-#define PACKET_LEN  11
-
+// create more descriptive names for serial interfaces
 #define SERCOM_USB   Serial
 #define SERCOM_UART  Serial1
 #define SERCOM_I2C   Wire
-#define AD0_VAL 	 1  // last bit of I2C address
+#define AD0_VAL      1  // last bit of I2C address
 
+// descriptive names for mode values
+#define MODE_STANDBY  0
+#define MODE_TEST     1
+
+/* GLOBAL VARIABLES ========================================================= */
+
+/**
+ * @brief
+ * IMU object, attached to IMU. Used to read data from IMU.
+ */
 ICM_20948_I2C IMU;
 
+/**
+ * @brief
+ * Stores the current mode of the ADCS.
+ * Possible values:
+ *   MODE_STANDBY
+ *   MODE_TEST
+ * 
+ * TODO: Add concurrency protections
+ */
 uint8_t mode;
 
-static void readUART(void* pvParameters);
-static void readSensors(void* pvParameters);
+/**
+ * @brief
+ * Stores a command currently being received from the satellite.
+ */
+TEScommand cmd_packet;
 
+/**
+ * @brief
+ * Stores sensor data to be sent to the satellite.
+ */
+ADCSdata data_packet;
+
+/* FUNCTION DECLARATIONS ==================================================== */
+
+// task function declarations
+static void readUART(void *pvParameters);
+static void writeUART(void *pvParameters);
+
+// "helper" functions from IMU demo code that print sensor data to serial
+// monitor over USB
+void printPaddedInt16b(int16_t val);
+void printRawAGMT(ICM_20948_AGMT_t agmt);
+void printFormattedFloat(float val, uint8_t leading, uint8_t decimals);
 void printScaledAGMT(ICM_20948_I2C *sensor);
 
+/* FUNCTION DEFINITIONS ===================================================== */
+
+/**
+ * @brief
+ * Since main is already defined by the Arduino framework, we will use the
+ * setup function as if it were main. Since setup runs only once, it
+ * essentially behaves the same as main.
+ */
 void setup()
 {
-	mode = 0;
+	mode = MODE_STANDBY;  // boot into standby mode
 
+
+	// initialize command and data packets to zeros
+	clearTEScommand(&cmd_packet);
+	clearADCSdata(&data_packet);
+
+	/**
+	 * Initialize UART connection to satellite
+	 * Baud rate: 115200
+	 * Data bits: 8
+	 * Parity: none
+	 */
 	SERCOM_USB.begin(115200);
-	while (!SERCOM_USB);
+	while (!SERCOM_USB);  // wait for initialization to complete
+	SERCOM_USB.write("USB interface initialized\n");
 
-	SERCOM_UART.begin(115200);
-	while (!SERCOM_UART);
+	/**
+	 * Initialize UART connection to satellite
+	 * Baud rate: 115200
+	 * Data bits: 8
+	 * Parity: odd (1 bit)
+	 */
+	SERCOM_UART.begin(115200, SERIAL_8O1);
+	while (!SERCOM_UART);  // wait for initialization to complete
+	SERCOM_USB.write("UART interface initialized\n");
 
+	/**
+	 * Initialize I2C connection to IMU
+	 * Clock: 400 kHz
+	 * IMU address: 0x68
+	 */
 	SERCOM_I2C.begin();
 	SERCOM_I2C.setClock(400000);
+	IMU.begin(SERCOM_I2C, AD0_VAL);
+	while (IMU.status != ICM_20948_Stat_Ok);  // wait for initialization to
+											  // complete
+	SERCOM_USB.write("IMU initialized\n");
 
-	// IMU.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
+	// initialization completed, notify satellite
+	data_packet.status = STATUS_HELLO;
+	// TODO: compute CRC
+	SERCOM_UART.write(data_packet.data, PACKET_LEN);
 
-	bool initialized = false;
-	while (!initialized)
-	{
-		IMU.begin(SERCOM_I2C, AD0_VAL);
-
-		SERCOM_USB.print(F("Initialization of the sensor returned: "));
-		SERCOM_USB.println(IMU.statusString());
-		if (IMU.status != ICM_20948_Stat_Ok)
-		{
-			SERCOM_USB.println("Trying again...");
-			delay(500);
-		}
-		else
-		{
-			initialized = true;
-		}
-	}
-
+	// instantiate tasks and start scheduler
 	xTaskCreate(readUART, "Read UART", 2048, NULL, 1, NULL);
-	xTaskCreate(readSensors, "Read Sensors", 2048, NULL, 1, NULL);
+	xTaskCreate(writeUART, "Write UART", 2048, NULL, 1, NULL);
 	SERCOM_USB.write("Tasks created\n");
 
 	vTaskStartScheduler();
+
+	// should never be reached if everything goes right
+	while (1);
 }
 
-static void readUART(void* pvParameters)
+/**
+ * @brief
+ * Polls the UART module for data. Processes data one byte at a time if the
+ * module reports that data is ready to be received.
+ * 
+ * @param[in] pvParameters  Unused but required by FreeRTOS. Program will not
+ * compile without this parameter. When a task is instantiated from this
+ * function, a set of initialization arguments or NULL is passed in as
+ * pvParameters, so pvParameters must be declared even if it is not used.
+ * 
+ * @return None
+ */
+static void readUART(void *pvParameters)
 {
-	uint8_t bytes_received = 0;
-	char message[COMMAND_LEN + 1];
-	
-	char cmd_str[8];
-
-	for (int i = 0; i < COMMAND_LEN+1; i++)
-	{
-		message[i] = 0;
-	}
+	uint8_t bytes_received = 0;  // number of consecutive bytes received from
+								 // satellite - used as index for cmd packet
+								 // char array
+	char cmd_str[8];  // used to print command value to serial monitor
 
 	while (1)
 	{
-		if (SERCOM_UART.available())
-		{
-			message[bytes_received] = SERCOM_UART.read();
+		if (SERCOM_UART.available())  // at least one byte is in the UART
+		{							  // receive buffer
+
+			cmd_packet.data[bytes_received] = SERCOM_UART.read();
 			bytes_received++;
 
-			if (bytes_received >= COMMAND_LEN)
+			if (bytes_received >= COMMAND_LEN)  // full command packet received
 			{
-				if (message[0] == 0xa0)
+				// TODO: verify CRC
+
+				if (cmd_packet.command == COMMAND_TEST)
 				{
-					mode = 1;
+					mode = MODE_TEST;  // enter test mode
 				}
 
-				if (message[0] == 0xc0)
+				if (cmd_packet.command == COMMAND_STANDBY)
 				{
-					mode = 0;
+					mode = MODE_STANDBY;  // enter standby mode
 				}
 
-				sprintf(cmd_str, "0x%02x", message[0]);
+				// convert int to string for USB monitoring
+				sprintf(cmd_str, "0x%02x", cmd_packet.command);
 
+				// print command value to USB
 				SERCOM_USB.print("Command received: ");
 				SERCOM_USB.print(cmd_str);
 				SERCOM_USB.print("\n");
 
-				if (message[0] == 0xa0)
+				if (cmd_packet.command == 0xa0)
 				{
 					SERCOM_USB.println("Entering test mode");
 				}
 
-				if (message[0] == 0xc0)
+				if (cmd_packet.command == 0xc0)
 				{
 					SERCOM_USB.println("Entering standby mode");
 				}
 
+				// reset index counter to zero for next command
 				bytes_received = 0;
 			}
 		}
 	}
 }
 
-static void readSensors(void* pvParameters)
+/**
+ * @brief
+ * Reads magnetometer and gyroscope values from IMU and writes them to UART
+ * every 0.5 seconds while ADCS is in test mode.
+ * 
+ * @param[in] pvParameters  Unused but required by FreeRTOS. Program will not
+ * compile without this parameter. When a task is instantiated from this
+ * function, a set of initialization arguments or NULL is passed in as
+ * pvParameters, so pvParameters must be declared even if it is not used.
+ * 
+ * @return None
+ */
+static void writeUART(void *pvParameters)
 {
-	char data[PACKET_LEN + 1];
-	ICM_20948_I2C* sensor_ptr = &IMU;
+	ICM_20948_I2C *sensor_ptr = &IMU;  // IMU data can only be accessed through
+									   // a pointer
 
-	for (int i = 0; i < PACKET_LEN+1; i++)
-	{
-		data[i] = 0;
-	}
+	data_packet.status = STATUS_OK;
 
-	data[0] = 0xaa;
-
-	data[1] = 3;
-	data[2] = 50;
-	data[3] = 1;
+	// use static dummy values for voltage, current, and motor speed until we
+	// have a device that can monitor them
+	data_packet.voltage = 0x01;
+	data_packet.current = 0x80;
+	data_packet.speed = 0x00;
 
 	while (1)
 	{
-		if (mode == 1)
+		if (mode == MODE_TEST)
 		{
 			if (IMU.dataReady())
 			{
-				IMU.getAGMT();
+				IMU.getAGMT();  // acquires data from sensor
+
+				// extract data from IMU object
+				data_packet.magX = (int8_t)sensor_ptr->magX();
+				data_packet.magY = (int8_t)sensor_ptr->magY();
+				data_packet.magZ = (int8_t)sensor_ptr->magZ();
+
+				data_packet.gyroX = (fixed5_3_t)sensor_ptr->gyrX();
+				data_packet.gyroY = (fixed5_3_t)sensor_ptr->gyrY();
+				data_packet.gyroZ = (fixed5_3_t)sensor_ptr->gyrZ();
+
+				// TODO: compute CRC
+
+				SERCOM_UART.write(data_packet.data, PACKET_LEN);  // send to TES
+				SERCOM_USB.write("Wrote to UART\n");
 				printScaledAGMT(&IMU);
-
-				data[4] = (int8_t) sensor_ptr->magX();
-				data[5] = (int8_t) sensor_ptr->magY();
-				data[6] = (int8_t) sensor_ptr->magZ();
-
-				data[7] = (int8_t) sensor_ptr->gyrX();
-				data[8] = (int8_t) sensor_ptr->gyrY();
-				data[9] = (int8_t) sensor_ptr->gyrZ();
-
-				data[10] = 0x01;
-
-				SERCOM_UART.write(data);
 			}
 		}
 
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		vTaskDelay(500 / portTICK_PERIOD_MS);
 	}
 }
 
+/**
+ * @brief
+ * Does nothing. Since we are using FreeRTOS, we will not use Arduino's loop
+ * function. However, the project will fail to compile if loop is not defined.
+ * Therefore, we define loop to do nothing.
+ * 
+ * TODO: Eliminate this function entirely? Even though it does nothing, it will
+ * still likely be called and consume clock cycles.
+ */
 void loop()
 {
 	// do nothing
 }
 
-// Below here are some helper functions to print the data nicely!
+/* IMU DEMO FUNCTIONS ======================================================= */
 
+/**
+ * @brief
+ * This function has something to do with printing IMU data over USB to the
+ * serial monitor. Other than that, I have no idea what it does. It came as part
+ * of the IMU demo code, and printScaledAGMT, which is used to validate data
+ * transmissions, relies on this function.
+ * 
+ * @param[in] val  Signed value to print
+ * 
+ * @return None
+ */
 void printPaddedInt16b(int16_t val)
 {
 	if (val > 0)
@@ -221,6 +314,17 @@ void printPaddedInt16b(int16_t val)
 	SERCOM_USB.print(abs(val));
 }
 
+/**
+ * @brief
+ * This function has something to do with printing IMU data over USB to the
+ * serial monitor. Other than that, I have no idea what it does. It came as part
+ * of the IMU demo code, and printScaledAGMT, which is used to validate data
+ * transmissions, relies on this function.
+ * 
+ * @param[in] agmt  An instance of the IMU data object
+ * 
+ * @return None
+ */
 void printRawAGMT(ICM_20948_AGMT_t agmt)
 {
 	SERCOM_USB.print("RAW. Acc [ ");
@@ -247,6 +351,19 @@ void printRawAGMT(ICM_20948_AGMT_t agmt)
 	SERCOM_USB.println();
 }
 
+/**
+ * @brief
+ * This function has something to do with printing IMU data over USB to the
+ * serial monitor. Other than that, I have no idea what it does. It came as part
+ * of the IMU demo code, and printScaledAGMT, which is used to validate data
+ * transmissions, relies on this function.
+ * 
+ * @param[in] val       Value to print
+ * @param[in] leading   Number of digits left of the decimal
+ * @param[in] decimals  Number of digits right of the decimal
+ * 
+ * @return None
+ */
 void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
 {
 	float aval = abs(val);
@@ -288,6 +405,15 @@ void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
 	}
 }
 
+/**
+ * @brief
+ * Prints IMU data over USB to the serial monitor. Converts raw data to a form
+ * that is readable by humans. Came as part of the IMU demo code.
+ * 
+ * @param[in] sensor  Pointer to IMU object
+ * 
+ * @return None
+ */
 void printScaledAGMT(ICM_20948_I2C *sensor)
 {
 	SERCOM_USB.print("Scaled. Acc (mg) [ ");
