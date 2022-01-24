ecba37e8a59e576e8c5a2fb97d839a50f36901ad
Parker Piedmont <pjpiedmont@gmail.com>
2022-01-24 15:48:43 -0800

    Changed build process to Python script

--- adcs/src/main.cpp
+++ adcs/src/main.cpp
@@ -1,7 +1,16 @@
-#include "hardware/samd51j20a.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+// #include <samd51j20a.h>
+// #include <samd.h>
+// #include <pio/samd51j20a.h>
+// #include <component/pio.h>
+// #include "hardware/samd51j20a.h"
 #include "FreeRTOS/FreeRTOS_SAMD51.h"
 
 int main()
 {
+	// PIOA->PIO_OER |= (1 << 0);
+
 	return 0;
 }
